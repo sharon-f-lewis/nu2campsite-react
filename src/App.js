e8c3baac6a1897fc8c61e9701a0f5eba704a873a
Sharon Lewis <sflewis827@yahoo.com>
2022-09-27 20:10:10 -0400

    CampsitesDirectoryPage and CampsiteDetail

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import CampsitesList from './features/campsites/CampsitesList';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import './App.css';
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='App'>
       <Header />
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );
