f0ea191632e46004081156e8bf5a7eda7dc9776c
Sharon Lewis <sflewis827@yahoo.com>
2022-10-12 20:27:31 -0400

    Redux for All

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,10 +1,12 @@
+import { useSelector } from 'react-redux';
 import { Col } from 'reactstrap';
 import Comment from './Comment';
 import CommentForm from './CommentForm';
 import { selectCommentsByCampsiteId } from './commentsSlice';
 
 const CommentsList = ({ campsiteId }) => {
-  const comments = selectCommentsByCampsiteId(campsiteId);
+  const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
+  console.log('comments:', comments);
 
     if (comments && comments.length > 0) {
       return (
